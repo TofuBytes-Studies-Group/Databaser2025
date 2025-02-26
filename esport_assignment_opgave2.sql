abd33f1be12dbcb33c96cec1c4275a2cc01d5e2b
Jamie G. Callan <jcallan32@gmail.com>
2025-02-26 13:31:14 +0100

    added 2.4
    Find spillere med flest sejre i en bestemt turnering.

--- esport_assignment_opgave2.sql
+++ esport_assignment_opgave2.sql
@@ -9,5 +9,14 @@ select count(*) from Tournament_Registrations where player_id='5';
 -- 2.3
 select * from Players p left join Tournament_Registrations tr on p.player_id = tr.player_id where tr.tournament_id = 3;
 
--- 2.4
-select * from Players p where (select MAX(count(*)) where p.player_id = m.winner_id);
+-- 2.4 
+WITH win_counts AS (
+    SELECT winner_id, COUNT(*) AS wins
+    FROM Matches
+    WHERE tournament_id = 5
+    GROUP BY winner_id
+)
+SELECT p.*
+FROM Players p
+JOIN win_counts wc ON p.player_id = wc.winner_id
+WHERE wc.wins = (SELECT MAX(wins) FROM win_counts);
